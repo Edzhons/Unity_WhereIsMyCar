b27ec0900050d589403c416486548b7a3e8fb161
Edzhons <edzuskrumins13@gmail.com>
2026-09-17 14:01:10 +0300

    random masinas/vietas/angle/flip / fine-tuning kodam

--- Assets/Scripts/GameObjectsScript.cs
+++ Assets/Scripts/GameObjectsScript.cs
@@ -5,7 +5,6 @@ public class GameObjectsScript : MonoBehaviour
     public GameObject garbageTruck;
     public GameObject medicine;
     public GameObject schoolBus;
-    // Pārējās mašīnas
     public GameObject eskavators;
     public GameObject b2;
     public GameObject cementaMasina;
@@ -16,12 +15,27 @@ public class GameObjectsScript : MonoBehaviour
     public GameObject traktors5;
     public GameObject ugunsdzeseji;
 
+
+    // =========================
+    // RANDOM SPAWN ARRAYS
+    // =========================
+
+    public GameObject[] cars;
+    public GameObject[] carPlaces;
+
+    public Transform[] carSpawnPoints;
+    public Transform[] carPlaceSpawnPoints;
+
+
     [HideInInspector]
     public Vector2 garbageTruckCoord;
+
     [HideInInspector]
     public Vector2 medicineCoord;
+
     [HideInInspector]
     public Vector2 schoolBusCoord;
+
     public Vector2 eskavatorsCoord;
     public Vector2 b2Coord;
     public Vector2 cementaMasinaCoord;
@@ -32,30 +46,134 @@ public class GameObjectsScript : MonoBehaviour
     public Vector2 traktors5Coord;
     public Vector2 ugunsdzesejiCoord;
 
+
     public Canvas canvas;
     public AudioSource carSoundSource;
     public AudioClip[] sounds;
 
     [HideInInspector]
     public bool inRightPlace = false;
+
     public static GameObject lastDragged = null;
     public static bool isDragging = false;
 
 
-
     void Awake()
     {
-        garbageTruckCoord = garbageTruck.GetComponent<RectTransform>().localPosition;
-        medicineCoord = medicine.GetComponent<RectTransform>().localPosition;
-        schoolBusCoord = schoolBus.GetComponent<RectTransform>().localPosition;
-        eskavatorsCoord = eskavators.GetComponent<RectTransform>().localPosition;
-        b2Coord = b2.GetComponent<RectTransform>().localPosition;
-        cementaMasinaCoord = cementaMasina.GetComponent<RectTransform>().localPosition;
-        e46Coord = e46.GetComponent<RectTransform>().localPosition;
-        e61Coord = e61.GetComponent<RectTransform>().localPosition;
-        policijaCoord = policija.GetComponent<RectTransform>().localPosition;
-        traktorsCoord = traktors.GetComponent<RectTransform>().localPosition;
-        traktors5Coord = traktors5.GetComponent<RectTransform>().localPosition;
-        ugunsdzesejiCoord = ugunsdzeseji.GetComponent<RectTransform>().localPosition;
+        RandomizeObjects();
+
+        garbageTruckCoord = garbageTruck.GetComponent<RectTransform>().anchoredPosition;
+        medicineCoord = medicine.GetComponent<RectTransform>().anchoredPosition;
+        schoolBusCoord = schoolBus.GetComponent<RectTransform>().anchoredPosition;
+        eskavatorsCoord = eskavators.GetComponent<RectTransform>().anchoredPosition;
+        b2Coord = b2.GetComponent<RectTransform>().anchoredPosition;
+        cementaMasinaCoord = cementaMasina.GetComponent<RectTransform>().anchoredPosition;
+        e46Coord = e46.GetComponent<RectTransform>().anchoredPosition;
+        e61Coord = e61.GetComponent<RectTransform>().anchoredPosition;
+        policijaCoord = policija.GetComponent<RectTransform>().anchoredPosition;
+        traktorsCoord = traktors.GetComponent<RectTransform>().anchoredPosition;
+        traktors5Coord = traktors5.GetComponent<RectTransform>().anchoredPosition;
+        ugunsdzesejiCoord = ugunsdzeseji.GetComponent<RectTransform>().anchoredPosition;
+    }
+
+
+    void RandomizeObjects()
+    {
+        // Shuffle car spawn points
+        Shuffle(carSpawnPoints);
+
+        // Shuffle car place spawn points
+        Shuffle(carPlaceSpawnPoints);
+
+
+        // Put each car into a random spawn point
+        for (int i = 0; i < cars.Length; i++)
+        {
+            RectTransform carRect =
+                cars[i].GetComponent<RectTransform>();
+
+            // Random position
+            carRect.anchoredPosition =
+                carSpawnPoints[i].GetComponent<RectTransform>().anchoredPosition;
+
+            // Random rotation
+            float randomRotation = Random.Range(-45f, 45f);
+            carRect.localRotation =
+                Quaternion.Euler(0f, 0f, randomRotation);
+
+            // Random size
+            float randomX = Random.Range(0.6f, 1.3f);
+            float randomY = Random.Range(0.6f, 1.3f);
+
+            carRect.localScale = new Vector3(
+                randomX,
+                randomY,
+                carRect.localScale.z
+            );
+
+            // Random mirror
+            bool randomMirror = Random.Range(0, 2) == 1;
+
+            if (randomMirror)
+            {
+                carRect.localScale = new Vector3(
+                    -carRect.localScale.x,
+                    carRect.localScale.y,
+                    carRect.localScale.z
+                );
+            }
+        }
+
+
+        // Put each car place into a random spawn point
+        for (int i = 0; i < carPlaces.Length; i++)
+        {
+            RectTransform placeRect =
+                carPlaces[i].GetComponent<RectTransform>();
+
+            // Random position
+            placeRect.anchoredPosition =
+                carPlaceSpawnPoints[i].GetComponent<RectTransform>().anchoredPosition;
+
+            // Random rotation
+            float randomRotation = Random.Range(-45f, 45f);
+            placeRect.localRotation =
+                Quaternion.Euler(0f, 0f, randomRotation);
+
+            // Random size
+            float randomX = Random.Range(0.6f, 1.3f);
+            float randomY = Random.Range(0.6f, 1.3f);
+
+            placeRect.localScale = new Vector3(
+                randomX,
+                randomY,
+                placeRect.localScale.z
+            );
+
+            // Random mirror
+            bool randomMirror = Random.Range(0, 2) == 1;
+
+            if (randomMirror)
+            {
+                placeRect.localScale = new Vector3(
+                    -placeRect.localScale.x,
+                    placeRect.localScale.y,
+                    placeRect.localScale.z
+                );
+            }
+        }
+    }
+
+
+    void Shuffle<T>(T[] array)
+    {
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+
+            T temp = array[i];
+            array[i] = array[randomIndex];
+            array[randomIndex] = temp;
+        }
     }
 }
--- Assets/Scripts/ObjectTransformationScript.cs
+++ Assets/Scripts/ObjectTransformationScript.cs
@@ -4,6 +4,9 @@ public class ObjectTransformationScript : MonoBehaviour
 {
     public GameObjectsScript gameObjectsScript;
 
+    public float minSize = 0.6f;
+    public float maxSize = 1.3f;
+    public float sizeSpeed = 0.003f;
 
     void Awake()
     {
@@ -29,41 +32,72 @@ public class ObjectTransformationScript : MonoBehaviour
 
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                if (GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.y < 0.9f)
+                float y = GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.y;
+
+                if (y < maxSize)
                 {
                     GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale =
-                    new Vector3(GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.x,
-                    GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.y + 0.001f, 1f);
+                        new Vector3(
+                            GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.x,
+                            y + sizeSpeed,
+                            1f
+                        );
                 }
             }
+
 
             if (Input.GetKey(KeyCode.DownArrow))
             {
-                if (GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.y > 0.3f)
+                float y = GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.y;
+
+                if (y > minSize)
                 {
                     GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale =
-                    new Vector3(GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.x,
-                    GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.y - 0.001f, 1f);
+                        new Vector3(
+                            GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.x,
+                            y - sizeSpeed,
+                            1f
+                        );
                 }
             }
+
 
             if (Input.GetKey(KeyCode.LeftArrow))
             {
-                if (GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.x > 0.3f)
+                RectTransform rect =
+                    GameObjectsScript.lastDragged.GetComponent<RectTransform>();
+
+                float x = Mathf.Abs(rect.localScale.x);
+
+                if (x > minSize)
                 {
-                    GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale =
-                    new Vector3(GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.x - 0.001f,
-                    GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.y, 1f);
+                    x -= sizeSpeed;
+
+                    rect.localScale = new Vector3(
+                        Mathf.Sign(rect.localScale.x) * x,
+                        rect.localScale.y,
+                        1f
+                    );
                 }
             }
 
+
             if (Input.GetKey(KeyCode.RightArrow))
             {
-                if (GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.x < 0.9f)
+                RectTransform rect =
+                    GameObjectsScript.lastDragged.GetComponent<RectTransform>();
+
+                float x = Mathf.Abs(rect.localScale.x);
+
+                if (x < maxSize)
                 {
-                    GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale =
-                    new Vector3(GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.x + 0.001f,
-                    GameObjectsScript.lastDragged.GetComponent<RectTransform>().localScale.y, 1f);
+                    x += sizeSpeed;
+
+                    rect.localScale = new Vector3(
+                        Mathf.Sign(rect.localScale.x) * x,
+                        rect.localScale.y,
+                        1f
+                    );
                 }
             }
 
