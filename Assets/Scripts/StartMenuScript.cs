13a5db311fd5654857d55261d7ddce24534f3cb1
Edzhons <edzuskrumins13@gmail.com>
2026-09-24 13:38:42 +0300

    Ir muzika

--- Assets/Scripts/StartMenuScript.cs
+++ Assets/Scripts/StartMenuScript.cs
@@ -1,15 +1,45 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class StartMenuScript : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AudioClip buttonSound;
+
     public void StartGame()
     {
-        SceneManager.LoadScene("Game");
+        StartCoroutine(StartGameWithSound());
     }
 
     public void QuitGame()
     {
+        StartCoroutine(QuitGameWithSound());
+    }
+
+
+    IEnumerator StartGameWithSound()
+    {
+        // Play button sound
+        audioSource.PlayOneShot(buttonSound);
+
+        // Wait for the sound to finish
+        yield return new WaitForSeconds(buttonSound.length);
+
+        // Load the game scene
+        SceneManager.LoadScene("CityScene");
+    }
+
+
+    IEnumerator QuitGameWithSound()
+    {
+        // Play button sound
+        audioSource.PlayOneShot(buttonSound);
+
+        // Wait for the sound to finish
+        yield return new WaitForSeconds(buttonSound.length);
+
+        // Quit the game
         Application.Quit();
 
 #if UNITY_EDITOR
